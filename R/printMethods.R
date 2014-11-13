7db34c0791456238c0f1eb8fa9773f120e6c3c9e
David Scott <d.scott@auckland.ac.nz>
2014-11-14 00:25:32 +1300

    Further checking of files

--- R/printMethods.R
+++ R/printMethods.R
@@ -1,14 +1,16 @@
-##--------------metabinDF-----------------##
+###--------------metabinDF-----------------###
 print.metabinDF <- function(x, ...) {
-   DF <- x$DF
-   colNames <- colnames(DF)
-   studyNames <- as.character(DF$study)
-   numStudies <- NROW(DF)
-   hetero <- x$hetero
-   print(list(title = x$title, subtitle = x$subtitle, colNames = colNames,
-   studyNames = studyNames, numStudies = numStudies, hetero = hetero))
+  DF <- x$DF
+  colNames <- colnames(DF)
+  studyNames <- as.character(DF$study)
+  numStudies <- NROW(DF)
+  hetero <- x$hetero
+  print(list(title = x$title, subtitle = x$subtitle, colNames = colNames,
+             studyNames = studyNames, numStudies = numStudies,
+             hetero = hetero))
 }
-##--------------metaPar-----------------##
+
+###--------------metaPar-----------------###
 print.metaPar <- function(x, ...) {
   print(unlist(x))
-}     
+}
